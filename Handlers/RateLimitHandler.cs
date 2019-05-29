959201f9d23d71a539af4e159de6a4ce38994ea1
Yucked <vuxdev@gmail.com>
2019-05-28 20:09:11 -0700

    Something to think about

---
+++ Handlers/RateLimitHandler.cs
@@ -0,0 +1,14 @@
+﻿using Frostbyte.Attributes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Frostbyte.Handlers
+{
+    [Service(ServiceLifetime.Singleton)]
+    public sealed class RateLimitHandler
+    {
+
+    }
+}
