90d7ee038e8159bc7e3f03bc80d26ad8d7e9df40
Yucked <vuxdev@gmail.com>
2019-05-28 18:03:11 -0700

    Changes in results

---
+++ Entities/Results/SoundCloudResult.cs
@@ -0,0 +1,36 @@
+using System.Text.Json.Serialization;
+
+namespace Frostbyte.Entities.Results
+{
+    public sealed class SoundCloudTrack
+    {
+        [JsonPropertyName("id")]
+        public long Id { get; set; }
+
+        [JsonPropertyName("permalink_url")]
+        public string PermalinkUrl { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("duration")]
+        public long Duration { get; set; }
+
+        [JsonPropertyName("artwork_url")]
+        public string ArtworkUrl { get; set; }
+
+        [JsonPropertyName("original_format")]
+        public string OriginalFormat { get; set; }
+
+        [JsonIgnore]
+        public TrackEntity ToTrack
+            => new TrackEntity
+            {
+                Id = $"{Id}",
+                Title = Title,
+                ThumbnailUrl = ArtworkUrl,
+                TrackLength = Duration,
+                Url = PermalinkUrl
+            };
+    }
+}
--- Entities/Results/YouTubeResult.cs
+++ Entities/Results/YouTubeResult.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
-namespace Frostbyte.Entities
+namespace Frostbyte.Entities.Results
 {
     public sealed class YouTubeResult
     {
