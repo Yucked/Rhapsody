5261c5bdacca8fc6f07fa84ad7922cbc08782d7c
Yucked <vuxdev@gmail.com>
2019-06-05 06:22:02 -0700

    Calling dispose disposes all guild connections

--- Websocket/WSClient.cs
+++ Websocket/WSClient.cs
@@ -19,7 +19,7 @@ namespace Frostbyte.Websocket
         private readonly IPEndPoint _endPoint;
 
         public event Func<IPEndPoint, ulong, Task> OnClosed;
-        public ConcurrentDictionary<ulong, GuildHandler> Guilds { get; }
+        public ConcurrentDictionary<ulong, GuildHandler> Guilds { get; private set; }
 
         public WsClient(WebSocketContext socketContext, ulong userId, int shards, IPEndPoint endPoint)
         {
@@ -71,6 +71,13 @@ namespace Frostbyte.Websocket
 
         public async ValueTask DisposeAsync()
         {
+            foreach (var (key, value) in Guilds)
+            {
+                await value.DisposeAsync().ConfigureAwait(false);
+                Guilds.TryRemove(key, out _);
+            }
+            Guilds.Clear();
+            Guilds = null;
             await _socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Disposing client.", CancellationToken.None).ConfigureAwait(false);
             _socket.Dispose();
         }
