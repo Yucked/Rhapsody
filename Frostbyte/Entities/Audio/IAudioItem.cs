cb72f0d80b830d29f476ae8d8dc144457e0f6986
TheMulti0 <multi@codeprecise.com>
2019-06-05 17:16:57 +0300

    Simplified IAudioItem

--- Frostbyte/Entities/Audio/IAudioItem.cs
+++ Frostbyte/Entities/Audio/IAudioItem.cs
@@ -7,9 +7,9 @@ namespace Frostbyte.Entities.Audio
     public interface IAudioItem
     {
         string Id { get; set; }
-        string Title { get; set; }
+        
         Author Author { get; set; }
-        string ThumbnailUrl { get; set; }
+
         string Url { get; set; }
     }
 }
