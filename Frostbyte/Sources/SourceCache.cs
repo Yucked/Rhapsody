a069a1f72e9734573c78733c763a6bdbb1649ed4
Yucked <vuxdev@gmail.com>
2019-06-06 19:15:42 -0700

    Need to figure out cache

--- Frostbyte/Sources/SourceCache.cs
+++ Frostbyte/Sources/SourceCache.cs
@@ -6,23 +6,6 @@ namespace Frostbyte.Sources
 {
     public class SourceCache
     {
-        private readonly ConcurrentDictionary<string, IAudioItem> _audioCache
-            = new ConcurrentDictionary<string, IAudioItem>();
 
-        public void AddToCache(IEnumerable<IAudioItem> audioItems)
-        {
-            foreach (var item in audioItems)
-            {
-                if (_audioCache.ContainsKey(item.Id))
-                    continue;
-
-                _audioCache.TryAdd(item.Id, item);
-            }
-        }
-
-        public bool TrySearchCache(string query, out IAudioItem audioItem)
-        {
-            return _audioCache.TryGetValue(query, out audioItem);
-        }
     }
 }
