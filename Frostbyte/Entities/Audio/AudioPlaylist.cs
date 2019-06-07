ebe430d01109961337d9f1f7628ab8ecb5e2948a
Yucked <vuxdev@gmail.com>
2019-06-06 19:12:58 -0700

    Renamed audio entities

---
+++ Frostbyte/Entities/Audio/AudioPlaylist.cs
@@ -0,0 +1,11 @@
+﻿namespace Frostbyte.Entities.Audio
+{
+    public sealed class AudioPlaylist
+    {
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public long Duration { get; set; }
+    }
+}
---
+++ Frostbyte/Entities/Audio/TrackAuthor.cs
@@ -0,0 +1,11 @@
+﻿namespace Frostbyte.Entities.Audio
+{
+    public sealed class TrackAuthor
+    {
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public string AvatarUrl { get; set; }
+    }
+}
