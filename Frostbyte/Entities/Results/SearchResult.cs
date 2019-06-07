1035d4ee683d5cbc78ee488b7ddf562cd0f5752b
Yucked <vuxdev@gmail.com>
2019-06-06 19:15:33 -0700

    Search result returns playlist info

---
+++ Frostbyte/Entities/Results/SearchResult.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Frostbyte.Entities.Audio;
+using Frostbyte.Entities.Enums;
+
+namespace Frostbyte.Entities.Results
+{
+    public sealed class SearchResult
+    {
+        public LoadType LoadType { get; set; }
+
+        public AudioPlaylist Playlist { get; set; }
+
+        public IEnumerable<AudioTrack> Tracks { get; set; }
+
+        public SearchResult()
+        {
+
+        }
+
+        public SearchResult(LoadType loadType = LoadType.NoMatches, IEnumerable<AudioTrack> tracks = default)
+        {
+            LoadType = loadType;
+            Tracks = tracks.ToList();
+        }
+    }
+}
