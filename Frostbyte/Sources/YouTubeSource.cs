ef22abb0072b334fa7f4b91be39ead952ab34865
Yucked <vuxdev@gmail.com>
2019-06-07 20:04:31 -0700

    YouTube get track, search and playlist

--- Frostbyte/Sources/YouTubeSource.cs
+++ Frostbyte/Sources/YouTubeSource.cs
@@ -1,12 +1,15 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Frostbyte.Attributes;
 using Frostbyte.Entities;
 using Frostbyte.Entities.Enums;
 using Frostbyte.Entities.Results;
+using Frostbyte.Extensions;
 using Frostbyte.Handlers;
 
 namespace Frostbyte.Sources
@@ -17,28 +20,120 @@ namespace Frostbyte.Sources
         public string Prefix { get; }
         public bool IsEnabled { get; }
 
+        private const string BASE_URL = "https://www.youtube.com";
+
+        private readonly Regex _idRegex;
+
         public YouTubeSource(Configuration config)
         {
             Prefix = "ytsearch";
             IsEnabled = config.Sources.EnableYouTube;
+            _idRegex = new Regex("(?!videoseries)[a-zA-Z0-9_-]{11,42}", RegexOptions.Compiled | RegexOptions.IgnoreCase);
         }
 
         public async ValueTask<SearchResult> SearchAsync(string query)
         {
-            var get = await HttpHandler.Instance
-                .WithUrl("https://www.youtube.com/search_ajax")
-                .WithParameter("style", "json")
-                .WithParameter("search_query", WebUtility.UrlEncode(query))
-                .GetBytesAsync().ConfigureAwait(false);
+            var search = new SearchResult();
+            var url = string.Empty;
 
-            var result = JsonSerializer.Parse<YouTubeResult>(get.Span);
-            var tracks = result.Video.Select(x => x.ToTrack).ToArray();
-            return new SearchResult(tracks.Length == 0 ? LoadType.NoMatches : LoadType.SearchResult, tracks);
+            switch (Uri.IsWellFormedUriString(query, UriKind.RelativeOrAbsolute))
+            {
+                case true:
+                    if (query.Contains("list="))
+                    {
+                        url = BASE_URL
+                            .WithPath("list_ajax")
+                            .WithParameter("style", "json")
+                            .WithParameter("action_get_list", "1")
+                            .WithParameter("list", "PLAYLIST_ID")
+                            .WithParameter("hl", "en");
+
+                        search.LoadType = LoadType.PlaylistLoaded;
+                    }
+                    else
+                    {
+                        url = BASE_URL
+                            .WithPath("search_ajax")
+                            .WithParameter("style", "json")
+                            .WithParameter("search_query", WebUtility.UrlEncode(query));
+
+                        search.LoadType = LoadType.TrackLoaded;
+                    }
+
+                    break;
+
+                case false:
+                    url = BASE_URL
+                        .WithPath("search_ajax")
+                        .WithParameter("style", "json")
+                        .WithParameter("search_query", WebUtility.UrlEncode(query));
+
+                    search.LoadType = LoadType.SearchResult;
+                    break;
+            }
+
+            var get = await HttpHandler.Instance
+                .GetBytesAsync(url).ConfigureAwait(false);
+
+            TryParseId(query, out var id);
+
+            switch (search.LoadType)
+            {
+                case LoadType.PlaylistLoaded:
+                    search.Playlist.Id = id;
+                    search.Playlist.Url = query;
+                    GetPlaylist(get.Span, ref search);
+                    break;
+
+                case LoadType.SearchResult:
+                    GetSearch(get.Span, ref search);
+                    break;
+
+                case LoadType.TrackLoaded:
+                    GetTrack(get.Span, id, ref search);
+                    break;
+            }
+
+            return search;
         }
 
         public async ValueTask<Stream> GetStreamAsync(string id)
         {
             throw new System.NotImplementedException();
+        }
+
+        private void GetSearch(ReadOnlySpan<byte> span, ref SearchResult result)
+        {
+            var search = JsonSerializer.Parse<YouTubeSearch>(span);
+            result.Tracks = search.Video.Select(x => x.ToTrack);
+        }
+
+        private void GetTrack(ReadOnlySpan<byte> span, string id, ref SearchResult result)
+        {
+            var search = JsonSerializer.Parse<YouTubeSearch>(span);
+            var track = new[] { search.Video.FirstOrDefault(x => x.Id == id).ToTrack };
+            result.Tracks = track;
+        }
+
+        private void GetPlaylist(ReadOnlySpan<byte> span, ref SearchResult result)
+        {
+            var playlist = JsonSerializer.Parse<YouTubePlaylist>(span);
+            result.Playlist.Name = playlist.Title;
+            result.Tracks = playlist.Videos.Select(x => x.ToTrack);
+            result.Playlist.Duration = result.Tracks.Sum(x => x.Duration);
+        }
+
+        private bool TryParseId(string url, out string id)
+        {
+            var match = _idRegex.Match(url);
+            if (!match.Success)
+            {
+                id = default;
+                return false;
+            }
+
+            id = match.Value;
+            return true;
         }
     }
 }
