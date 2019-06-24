25c1650aba74661eef895c054fd4a4e49705d7ac
Yucked <vuxdev@gmail.com>
2019-06-24 13:27:44 -0700

    Port check just in case

--- Websocket/WSServer.cs
+++ Websocket/WSServer.cs
@@ -11,24 +11,25 @@ using Frostbyte.Entities.Packets;
 using Frostbyte.Extensions;
 using System.Text.Json;
 using Frostbyte.Entities.Enums;
+using System.Net.NetworkInformation;
 
 namespace Frostbyte.Websocket
 {
-    public sealed class WsServer : IAsyncDisposable
+    public sealed class WSServer : IAsyncDisposable
     {
         private readonly Configuration _config;
         private readonly SourceHandler _sources;
-        private readonly ConcurrentDictionary<IPEndPoint, WsClient> _clients;
+        private readonly ConcurrentDictionary<IPEndPoint, WSClient> _clients;
         private readonly HttpListener _listener;
         private readonly CancellationTokenSource _mainCancellation, _wsCancellation, _statsCancellation;
 
         private CancellationTokenSource _receiveCancellation;
         private Task _statsSenderTask;
 
-        public WsServer()
+        public WSServer()
         {
             _listener = new HttpListener();
-            _clients = new ConcurrentDictionary<IPEndPoint, WsClient>();
+            _clients = new ConcurrentDictionary<IPEndPoint, WSClient>();
             _wsCancellation = new CancellationTokenSource();
             _statsCancellation = new CancellationTokenSource();
             _mainCancellation = CancellationTokenSource.CreateLinkedTokenSource(_wsCancellation.Token, _statsCancellation.Token);
@@ -51,12 +52,26 @@ namespace Frostbyte.Websocket
 
         public async Task InitializeAsync()
         {
-            LogHandler<WsServer>.Log.Information("TLS11, TLS12 & TLS13 set as security protocol.");
+            LogHandler<WSServer>.Log.Information("TLS11, TLS12 & TLS13 set as security protocol.");
             ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12 | SecurityProtocolType.Tls13;
+
+            LogHandler<WSServer>.Log.Information($"Checking port {_config.Port} status.");
+            var globalProps = IPGlobalProperties.GetIPGlobalProperties();
+            var activeListners = globalProps.GetActiveTcpListeners();
+            var random = new Random();
+            foreach (var listner in activeListners)
+            {
+                if (listner.Port == _config.Port)
+                {
+                    LogHandler<WSServer>.Log.Warning($"{_config.Port} is already in use. Picking a random port.");
+                    _config.Port = random.Next(IPEndPoint.MinPort, IPEndPoint.MaxPort);
+                    Singleton.Update<Configuration>(_config);
+                }
+            }
 
             _listener.Prefixes.Add(_config.Url);
             _listener.Start();
-            LogHandler<WsServer>.Log.Information($"Server started on {_config.Url}.");
+            LogHandler<WSServer>.Log.Information($"Server started on {_config.Url}.");
 
             _statsSenderTask = Task.Run(CollectStatsAsync, _statsCancellation.Token);
             while (!_wsCancellation.IsCancellationRequested)
@@ -77,7 +92,7 @@ namespace Frostbyte.Websocket
                 switch (localPath)
                 {
                     case "/tracks":
-                        LogHandler<WsServer>.Log.Debug($"Incoming http request from {remoteEndPoint}.");
+                        LogHandler<WSServer>.Log.Debug($"Incoming http request from {remoteEndPoint}.");
                         if (context.Request.Headers.Get("Password") != _config.Password)
                         {
                             response.Reason = "Password header doesn't match value specified in configuration";
@@ -99,7 +114,7 @@ namespace Frostbyte.Websocket
 
                         response.Operation = OperationType.REST;
                         await context.SendResponseAsync(response).ConfigureAwait(false);
-                        LogHandler<WsServer>.Log.Debug($"Replied to {remoteEndPoint} with {response.Reason}.");
+                        LogHandler<WSServer>.Log.Debug($"Replied to {remoteEndPoint} with {response.Reason}.");
                         break;
 
                     case "/":
@@ -110,19 +125,19 @@ namespace Frostbyte.Websocket
                             return;
                         }
 
-                        LogHandler<WsServer>.Log.Debug($"Incoming websocket request coming from {remoteEndPoint}.");
+                        LogHandler<WSServer>.Log.Debug($"Incoming websocket request coming from {remoteEndPoint}.");
 
                         var wsContext = await context.AcceptWebSocketAsync(default).ConfigureAwait(false);
-                        var wsClient = new WsClient(wsContext, remoteEndPoint);
+                        var wsClient = new WSClient(wsContext, remoteEndPoint);
                         wsClient.OnClosed += OnClosed;
                         _clients.TryAdd(remoteEndPoint, wsClient);
                         _ = wsClient.ReceiveAsync(_receiveCancellation);
 
-                        LogHandler<WsServer>.Log.Information($"Websocket connection opened from {remoteEndPoint}.");
+                        LogHandler<WSServer>.Log.Information($"Websocket connection opened from {remoteEndPoint}.");
                         break;
 
                     default:
-                        LogHandler<WsServer>.Log.Warning($"{remoteEndPoint} requested an unknown path: {context.Request.Url}.");
+                        LogHandler<WSServer>.Log.Warning($"{remoteEndPoint} requested an unknown path: {context.Request.Url}.");
                         response.Reason = "You are trying to access an unknown endpoint.";
                         await context.SendResponseAsync(response).ConfigureAwait(false);
                         break;
@@ -133,7 +148,7 @@ namespace Frostbyte.Websocket
                 response.IsSuccess = false;
                 response.Reason = $"Frostbyte threw an inner exception: {ex?.InnerException?.Message ?? ex?.Message}";
                 await context.SendResponseAsync(response).ConfigureAwait(false);
-                LogHandler<WsServer>.Log.Error(ex);
+                LogHandler<WSServer>.Log.Error(ex);
             }
             finally
             {
