9501cb66a8afead2043330e11463998b97b57bbd
Yucked <vuxdev@gmail.com>
2019-06-25 21:20:57 -0700

    Need to cramp all the other streams in one somehow or follow D#+ exampe

--- Audio/AudioStream.cs
+++ Audio/AudioStream.cs
@@ -2,7 +2,6 @@
 using NAudio.Wave.SampleProviders;
 using System;
 using System.IO;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace Frostbyte.Audio
@@ -11,24 +10,6 @@ namespace Frostbyte.Audio
     {
         private const int RESAMPLE_RATE = 48000;
         private const int RESAMPLE_CHANNELS = 2;
-
-        [DllImport("libsodium", EntryPoint = "crypto_secretbox_easy", CallingConvention = CallingConvention.Cdecl)]
-        private static unsafe extern int SecretBoxEasy(byte* output, byte* input, long inputLength, byte[] nonce, byte[] secret);
-
-        public static int LibSodiumEncrypt(byte[] input, int inputOffset, int inputLength, byte[] output, int outputOffset, byte[] nonce, byte[] secret)
-        {
-            unsafe
-            {
-                fixed (byte* inPtr = input)
-                fixed (byte* outPtr = output)
-                {
-                    var error = SecretBoxEasy(outPtr + outputOffset, inPtr + inputOffset, inputLength, nonce, secret);
-                    if (error != 0)
-                        throw new Exception($"Sodium Error: {error}");
-                    return inputLength + 16;
-                }
-            }
-        }
 
         public static IWaveProvider GetPCMStream(Stream stream)
         {
@@ -51,13 +32,6 @@ namespace Frostbyte.Audio
 
         public static async Task CreatePCMStreamAsync()
         {
-
-        }
-
-        private static async Task SodiumEncryptAsync(byte[] buffer, int offset, int count, byte[] nonce, byte[] secret)
-        {
-            Buffer.BlockCopy(buffer, offset, nonce, 0, 12);
-            count = LibSodiumEncrypt(buffer, offset * 12, count - 12, buffer, 12, nonce, secret);
 
         }
     }
---
+++ Audio/MainStream.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Frostbyte.Audio
+{
+    public sealed class MainStream
+    {
+
+    }
+}
