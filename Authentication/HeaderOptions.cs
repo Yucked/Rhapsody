9f5be74e31d62935d85afc7c3d74b6c9080aec4e
Yucked <vuxdev@gmail.com>
2019-09-29 14:38:58 -0700

    Cleanup for authentication related stuff

--- Authentication/HeaderOptions.cs
+++ Authentication/HeaderOptions.cs
@@ -1,17 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authentication;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Concept.Authentication
 {
-    public class HeaderOptions : AuthenticationSchemeOptions
+    public sealed class HeaderOptions : AuthenticationSchemeOptions
     {
-        /// <summary>
-        /// Is the value in the Header to valid the authorize:
-        /// Authorization: MyInvenciblePassword
-        /// </summary>
-        public string Authorization { get; set; } = "MyInvenciblePassword";
+        public string Authorization { get; set; }
     }
 }
