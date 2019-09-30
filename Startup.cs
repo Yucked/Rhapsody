1b109cd57590a2421325771096571a98ba8db957
Yucked <vuxdev@gmail.com>
2019-09-30 07:10:33 -0700

    Renaming of Settings and what not

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
+using Concept.Caches;
 using Concept.Controllers;
 using Concept.Middlewares;
-using Concept.WebSockets;
+using Concept.Options;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,12 +12,12 @@ namespace Concept
     public sealed class Startup
     {
         private readonly IConfiguration _configuration;
-        private readonly Settings _settings;
+        private readonly ApplicationOptions _applicationOptions;
 
         public Startup(IConfiguration configuration)
         {
             _configuration = configuration;
-            _settings = _configuration.Get<Settings>();
+            _applicationOptions = _configuration.Get<ApplicationOptions>();
         }
 
         public void ConfigureServices(IServiceCollection services)
@@ -24,16 +25,19 @@ namespace Concept
             services.AddTransient<ClientsCache>();
             services.AddSingleton<Theoretical>();
             services.AddSingleton<WebSocketController>();
-            services.Configure<Settings>(_configuration);
+            services.Configure<ApplicationOptions>(_configuration);
             services.AddControllers();
             services.AddAuthentication()
-                .UseHeaderAuthentication(options => options.Authorization = _settings.Authorization);
+                .UseHeaderAuthentication(options => options.Authorization = _applicationOptions.Authorization);
+
+            if (_applicationOptions.CacheOptions.IsEnabled)
+                services.AddSingleton<ResponsesCache>();
         }
 
         public void Configure(IApplicationBuilder app)
         {
             app.UseMiddleware(typeof(ExceptionMiddleware));
-            app.UseMiddleware(typeof(WebSocketMiddleware), _settings.Authorization);
+            app.UseMiddleware(typeof(WebSocketMiddleware), _applicationOptions.Authorization);
 
             app.UseAuthentication();
             app.UseAuthorization();
