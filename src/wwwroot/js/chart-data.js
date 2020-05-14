60cee305c2d1d135e7ac0fa925098434f4c98501
Yucked <mail@dvlp.nill.sh>
2020-05-14 15:35:17 -0700

    Start writing the frontend?

---
+++ src/wwwroot/js/chart-data.js
@@ -0,0 +1,23 @@
+function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+
+    chart.options = {
+        response: true
+    };
+    chart.update();
+}
+
+function removeData(chart) {
+    chart.data.labels.pop();
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.pop();
+    });
+
+    chart.options = {
+        response: true
+    };
+    chart.update();
+}
