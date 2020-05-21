191115193d75c1bc22a7e6771da52b894a8e799c
Yucked <mail@dvlp.nill.sh>
2020-05-21 12:35:13 -0700

    Remove the Create method

--- src/Controllers/PlayerController.cs
+++ src/Controllers/PlayerController.cs
@@ -1,14 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
-using Rhapsody.Controllers.Payloads;
+using Microsoft.Extensions.Logging;
+using Rhapsody.Payloads;
+using Rhapsody.Payloads.Inbound;
 
 namespace Rhapsody.Controllers {
 	[Route("api/[controller]"), ApiController]
 	public sealed class PlayerController : ControllerBase {
 		private readonly IMemoryCache _memoryCache;
+		private readonly ILoggerFactory _loggerFactory;
 
-		public PlayerController(IMemoryCache memoryCache) {
+		public PlayerController(IMemoryCache memoryCache, ILoggerFactory loggerFactory) {
 			_memoryCache = memoryCache;
+			_loggerFactory = loggerFactory;
 		}
 
 		[HttpGet("{guildId}")]
@@ -17,16 +21,6 @@ namespace Rhapsody.Controllers {
 				return NotFound();
 			}
 
-			return Ok(player);
-		}
-
-		[HttpPost("{guildId}")]
-		public IActionResult Create(ulong guildId, GuildPlayer player) {
-			if (_memoryCache.TryGetValue(guildId, out _)) {
-				return Conflict();
-			}
-
-			_memoryCache.Set(guildId, player);
 			return Ok(player);
 		}
 
@@ -42,8 +36,21 @@ namespace Rhapsody.Controllers {
 
 		[HttpPost("{guildId}")]
 		public IActionResult HandlePayload(ulong guildId, BasePayload payload) {
-			if (!_memoryCache.TryGetValue(guildId, out _)) {
+			if (!ModelState.IsValid) {
+				return BadRequest(ModelState);
+			}
+
+			if (payload.Op != PayloadOp.Connect && !_memoryCache.TryGetValue(guildId, out _)) {
 				return NotFound();
+			}
+
+			switch (payload.Op) {
+				case PayloadOp.Connect:
+					var logger = _loggerFactory.CreateLogger<GuildPlayer>();
+					var guildPlayer = new GuildPlayer(payload as ConnectPayload, "", logger);
+
+					_memoryCache.Set(guildId, guildPlayer);
+					break;
 			}
 
 			return Ok();
