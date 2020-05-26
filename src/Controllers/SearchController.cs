02eeff89f0dd175e34b23ed94e81b97f4dde11bd
Yucked <mail@dvlp.nill.sh>
2020-05-25 20:00:01 -0400

    Completed SearchController.cs / Need to add cache stuff later

--- src/Controllers/SearchController.cs
+++ src/Controllers/SearchController.cs
@@ -1,24 +1,79 @@
+using System;
 using System.Threading.Tasks;
+using Dysc;
+using Dysc.Providers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Rhapsody.Payloads.Outbound;
 
 namespace Rhapsody.Controllers {
 	[Route("api/[controller]"), ApiController, Produces("application/json")]
 	public sealed class SearchController : ControllerBase {
+		private readonly DyscClient _dyscClient;
+		private readonly ILogger _logger;
+
+		public SearchController(DyscClient dyscClient, ILogger<SearchController> logger) {
+			_dyscClient = dyscClient;
+			_logger = logger;
+		}
+
 		[HttpGet("/youtube")]
 		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 		[ProducesResponseType(StatusCodes.Status400BadRequest)]
-		public Task<ActionResult<object>> GetYouTubeAsync(string query) {
-			return SearchAsync(default, default);
+		public ValueTask<ActionResult<RestResponse>> GetYouTubeAsync(string query) {
+			return SearchAsync(ProviderType.YouTube, query);
 		}
 
 		[HttpGet("/soundcloud")]
-		public Task GetSoundCloudAsync(string query) {
-			return SearchAsync(default, default);
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		public ValueTask<ActionResult<RestResponse>> GetSoundCloudAsync(string query) {
+			return SearchAsync(ProviderType.SoundCloud, query);
 		}
 
-		private async Task<ActionResult<object>> SearchAsync(object providerType, string query) {
-			return default;
+		[HttpGet("/bandcamp")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		public ValueTask<ActionResult<RestResponse>> GetBandCampAsync(string query) {
+			return SearchAsync(ProviderType.BandCamp, query);
+		}
+
+		[HttpGet("/hearthisat")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		public ValueTask<ActionResult<RestResponse>> GetHearThisAtAsync(string query) {
+			return SearchAsync(ProviderType.HearThisAt, query);
+		}
+
+		[HttpGet("/http")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		public ValueTask<ActionResult<RestResponse>> GetHttpAsync(string query) {
+			return !Uri.IsWellFormedUriString(query, UriKind.Absolute)
+				? new ValueTask<ActionResult<RestResponse>>(RestResponse.Error("Query must be an absolute URI string."))
+				: SearchAsync(ProviderType.Http, query);
+		}
+
+		private async ValueTask<ActionResult<RestResponse>> SearchAsync(ProviderType providerType, string query) {
+			if (string.IsNullOrWhiteSpace(query)) {
+				return RestResponse.Error("Query must not be empty.");
+			}
+
+			try {
+				var provider = _dyscClient.GetProvider(providerType);
+				var searchResponse = await provider.SearchAsync(query);
+				return RestResponse.Ok(searchResponse);
+			}
+			catch (Exception exception) {
+				_logger.LogCritical(exception, exception.StackTrace);
+				return RestResponse.Error(exception.Message);
+			}
 		}
 	}
 }
